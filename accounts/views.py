82d4df10a6585f82052b4a0a1d42bbf312ff1144
Youssef Moris <abc.abcd202020@gmail.com>
2025-05-21 01:04:11 +0300

    added first name, last name, email for the signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
+from .forms import CustomUserCreationForm
+
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        form.fields['password1'].help_text = "Password must be at least 8 characters and not entirely numeric."
-        form.fields['password2'].help_text = "Enter the same password as before, for verification."
-        form.fields['username'].widget.attrs['placeholder'] = 'Username'
-        form.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        form.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
-        form.fields['password1'].help_text = "Password must be at least 8 characters and not entirely numeric."
-        form.fields['password2'].help_text = "Enter the same password as before, for verification."
-        form.fields['username'].widget.attrs['placeholder'] = 'Username'
-        form.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        form.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        form = CustomUserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -35,6 +28,7 @@ def login_view(request):
         form.fields['username'].widget.attrs['placeholder'] = 'Username'
         form.fields['password'].widget.attrs['placeholder'] = 'Password'
     return render(request, 'login.html', {'form': form})
+
 
 def logout_view(request):
     if request.method == 'POST':
