bd457092a3cccdf1c5c2e2bbe2058c9fb37097e5
Sonosy <abdelrahmansinosi@gmail.com>
2025-05-21 22:57:52 +0300

    Fixed the admin panel page url

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin-dashboard'),
     path('add_recipe/', views.add_recipe, name='add_recipe'),
     path('delete_recipe/<int:recipe_id>/', views.delete_recipe, name='delete_recipe'),
     path('edit_recipe/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),
